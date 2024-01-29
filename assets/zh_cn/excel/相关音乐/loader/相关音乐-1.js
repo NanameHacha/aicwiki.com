cdde37d5601a283f6130a67712657f4c8241cdb3
Cicini <shirocicini@outlook.com>
2024-01-29 18:31:04 +0000

    docs: delete assets/zh_cn/excel/相关音乐/loader/相关音乐-1.js

--- assets/zh_cn/excel/相关音乐/loader/相关音乐-1.js
+++
@@ -1,6 +0,0 @@
-import {loadRemoteFile} from "/web-static/javascript/main.js"
-window.addEventListener('load', (/*event*/) => {
-  let url ="https://aicwiki.com/assets/zh_cn/excel/相关音乐/相关音乐-1.xlsx";
-  let elementId = "相关音乐-1";
-  loadRemoteFile(url,elementId);
-});
